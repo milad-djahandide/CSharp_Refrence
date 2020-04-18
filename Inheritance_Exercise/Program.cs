9b189e36d13a1860f97e54315b859d974745091a
milad-djahandide <62598836+milad-djahandide@users.noreply.github.com>
2020-04-18 13:36:00 +0430

    Inheritance Exircise Added

---
+++ Inheritance_Exercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+
+namespace Inheritance_Exercise
+{
+
+    public class Vehicle
+    {
+        public string RegistrationNumber { get; }
+        public Point CurrentPosition { get; set; }
+
+        public Vehicle(string registrationNumber)
+        {
+            RegistrationNumber = registrationNumber;
+            Console.WriteLine("Vehicle initialized with registration number : {0}", registrationNumber);
+        }
+
+        public void Move(Point movingVector)
+        {
+            CurrentPosition = new Point(CurrentPosition.X + movingVector.X, CurrentPosition.Y + movingVector.Y);
+            Console.WriteLine("Vehicle Moved and Current position is {0}", CurrentPosition );
+        }
+    }
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber) :
+            base(registrationNumber)
+        {
+            Console.WriteLine("Car initialized with registeration number : {0}", registrationNumber);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("112314");
+            Vehicle vhcl = car; //upcasting
+
+            vhcl.Move(new Point(5, 10));
+
+            Car car2 = (Car)vhcl; //downcasting
+            car2.Move(new Point(12,-8));
+        }
+    }
+}
---
+++ MoshHamedaniTopics/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+
+namespace MoshHamedaniTopics
+{
+
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+    }
+
+
+}
